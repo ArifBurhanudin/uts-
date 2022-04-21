748da7386358b2920ba142ac6b576c5dee1ca42a
ArifBurhanudin <104148235+ArifBurhanudin@users.noreply.github.com>
2022-04-21 20:55:24 +0700

    Add files via upload

---
+++ uts_arifburhanudin.py
@@ -0,0 +1,11 @@
+print("Nama : Arif Burhanudin")
+print("Nim : 191011402164")
+# Nomer 2 Contoh If Else
+
+nilai = 90
+print('Nilai anda adalah:', nilai, '\n')
+
+if nilai >= 70:
+    print('Selamat, anda lulus!')
+else:
+    print('Maaf, anda tidak lulus.')
